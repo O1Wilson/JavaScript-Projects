aeb2d21eb3eb8861ce11bb8eb49bfa8c7dca431c
O1Wilson <Badgermouse583@gmail.com>
2024-02-10 13:07:06 -0600

    Added things

---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,12 @@
+var myVariable = 42;
+document.write("The data type of myVariable is: " + typeof myVariable);
+
+document.write("Infinity:" + (2E310) + "<br>");
+document.write("Negative Infinity: " + (-3E310) + "<br>");
+document.write("Comparison result (10 < 2): " + (10 < 2) + "<br>");
+X = 10;
+Y = "10";
+document.write("Variable comparison (X == Y):" + (X == Y) + "<br>");
+console.log("Console log: " +(2 + 2));
+document.write("Logical AND (10 > 5 && 10 < 15:" + (10 > 5 && 10 < 15) + "<br>");
+document.write("Logical OR (2 > 10 || 10 > 2:" + (2 > 10 || 10 > 2) + "<br>");
