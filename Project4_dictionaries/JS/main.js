8a29a325e1b4a436e860b7991ca9c5d4f53b6813
O1Wilson <Badgermouse583@gmail.com>
2024-02-10 12:27:17 -0600

    Project new

---
+++ Project4_dictionaries/JS/main.js
@@ -0,0 +1,15 @@
+var myDictionary = {
+    key1: "Value 1",
+    key2: "Value 2",
+    key3: "Value 3",
+};
+delete myDictionary.key3
+document.getElementById("dictionary").innerHTML = myDictionary.key3
+
+function displayDictionaryValue() {
+    var keyToDisplay = "key2"; 
+
+    var valueToDisplay = myDictionary[keyToDisplay];
+
+    document.getElementById("Dictionary").innerHTML = "The value for " + keyToDisplay + " is: " + valueToDisplay;
+}
