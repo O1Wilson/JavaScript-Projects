b182e34edc9b0b94aa3f461c6ac5ab22210df0d5
O1Wilson <Badgermouse583@gmail.com>
2024-02-12 17:05:51 -0600

    Update main.js

--- Project9_countdown_slideshow/JS/main.js
+++ Project9_countdown_slideshow/JS/main.js
@@ -1,10 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+
 function countdown() {
 	var seconds = document.getElementById("seconds").value;
 	
 	function tick() {
 		seconds = seconds - 1;
 		timer.innerHTML = seconds;
-		var time = setTimeout = tick, 1000;
+		var time = setTimeout(tick, 1000);
 		if (seconds == -1) {
 			alert("Time's up!");
 			clearTimeout(time);
@@ -40,3 +42,21 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+showSlides(1);
+
+document.querySelector(".prev").addEventListener("click", function () {
+    plusSlides(-1);
+  });
+
+  document.querySelector(".next").addEventListener("click", function () {
+    plusSlides(1);
+  });
+
+  let dots = document.getElementsByClassName("dot");
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].addEventListener("click", function () {
+      currentSlide(i + 1);
+    });
+  }
+});
