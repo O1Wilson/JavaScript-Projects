60a0c23dec407ffdb095b27de0c0c6d58a3d3b09
O1Wilson <Badgermouse583@gmail.com>
2024-02-12 14:20:20 -0600

    Update main.js

--- Project5_comparisons_type_coercion/JS/main.js
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -7,6 +7,13 @@ document.write("Comparison result (10 < 2): " + (10 < 2) + "<br>");
 X = 10;
 Y = "10";
 document.write("Variable comparison (X == Y):" + (X == Y) + "<br>");
+document.write("Strict variable comparison (X === Y): " + (X === Y) + "<br>"); // Added === for strict comparison
 console.log("Console log: " +(2 + 2));
 document.write("Logical AND (10 > 5 && 10 < 15:" + (10 > 5 && 10 < 15) + "<br>");
 document.write("Logical OR (2 > 10 || 10 > 2:" + (2 > 10 || 10 > 2) + "<br>");
+
+document.write("Negation of true (Logical NOT): " + !true + "<br>"); // Added logical NOT (!)
+
+var myString = "Hello";
+var myInt = 5;
+document.write("Combining string and int: " + (myString + myInt) + "<br>"); // Combining string with int
