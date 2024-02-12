e5d1fbbca8391a4a05a006a859899dc7acf5ea1f
O1Wilson <Badgermouse583@gmail.com>
2024-02-12 17:48:01 -0600

    Update main.js

--- Project7_scope_time_fuction/JS/main.js
+++ Project7_scope_time_fuction/JS/main.js
@@ -1,31 +1,39 @@
 var X = 10;
-function Add_numbers_1() {
-	document.write(20 + X + "<br>");
-}
-function Add_numbers_2() {
-	document.write(X + 100);
-}
-Add_numbers_1();
-Add_numbers_2();
 
 function Add_numbers_1() {
 	var Y = 20;
 	document.write(40 + Y + "<br>");
 }
+
 function Add_numbers_2() {
 	var Y = 10;
 	document.write(Y + 100);
 }
+
 Add_numbers_1();
 Add_numbers_2();
 
+function getCurrentTime() {
+    var now = new Date();
+    return now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+}
+
+function Time_function() {
+	console.log("This is a time function");
+	console.log("Current time: " + getCurrentTime());  // Intentional error
+}
+
+Time_function();
+
 function Age_Function() {
-	Age = document.getElementById("Age").value;
+	var Age = document.getElementById("Age").value;
+	var Vote;
+
 	if (Age >= 18) {
 		Vote = "You are old enough to vote!";
-	}
-	else {
+	} else {
 		Vote = "You are not old enough to vote!";
 	}
+
 	document.getElementById("How_old_are_you?").innerHTML = Vote;
 }
