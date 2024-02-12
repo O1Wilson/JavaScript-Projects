2bfbce2717c3dbec8fe677bc90ea7dbf3fe40d8a
O1Wilson <Badgermouse583@gmail.com>
2024-02-12 17:09:56 -0600

    Update main.js

--- Project10_loops_array/JS/main.js
+++ Project10_loops_array/JS/main.js
@@ -12,10 +12,10 @@ var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flu
 var Content = "";
 var Y;
 function for_Loop() {
-	for (Y = 0; Y < Instruments.length; Y++); {
-	Content += Instruments [Y] + "<br>";
-	}
-	document.getElementById("List_of_Instruments").innerHTML
+    for (Y = 0; Y < Instruments.length; Y++) {
+        Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content;
 }
 
 function cat_pics() {
