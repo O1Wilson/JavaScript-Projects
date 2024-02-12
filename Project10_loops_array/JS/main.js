d91a6258a47ae36bea4c60e4002bcf55bffb1678
O1Wilson <Badgermouse583@gmail.com>
2024-02-12 14:33:26 -0600

    changed

--- Project10_loops_array/JS/main.js
+++ Project10_loops_array/JS/main.js
@@ -53,4 +53,4 @@ let car = {
 		return "The car is a " + this.year + this.color + this.make + this.model;
 	}
 };
-document.getElementById("car_Object").innerHTML = car.description();
+document.getElementById("Car_Object").innerHTML = car.description();
